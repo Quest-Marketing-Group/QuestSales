a901034844112eaf05d221f5b147256b9c453e5f
Rebecca Velez <contact@rebeccavelez.dev>
2022-12-06 15:16:26 -0500

    Work on shopping cart

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,3 +1,5 @@
+
+
 /* Set rates + misc */
 var taxRate = 0.05;
 var shippingRate = 15.00;
@@ -69,6 +71,14 @@ function updateQuantity(qty, quantityInput)
             $(this).fadeIn(fadeTime);
         });
     });
+}
+
+function calc(n) {
+    var price = document.getElementsByClassName("item_price")[n].innerHTML;
+    var noTickets = document.getElementsByClassName("num")[n].value;
+    var total = parseFloat(price) * noTickets;
+    if (!isNaN(total))
+        document.getElementsByClassName("product-total")[n].innerHTML = total;
 }
 
 
